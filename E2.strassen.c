bd35fbd75a3cd2d23408a692859a71f6f29f3515
midiachok <162707182+midiachok@users.noreply.github.com>
2024-06-09 18:15:05 +0200

    funkcia

--- E2.strassen.c
+++ E2.strassen.c
@@ -33,7 +33,7 @@ void fill(MAT* mat) {
      srand(time(0));
      
      do{
-          printf("chcete zapisat vsetky prvky matice: 1 - z klavesnice, 2 - auto?");
+          printf("ako chcete zapisat vsetky prvky matice? 1 - z klavesnice, 2 - automaticky");
           scanf("%d",&choice);
      } while (choice != 1 && choice != 2);
      
@@ -65,24 +65,60 @@ void show(MAT mat) {
 
 }
 
+void mat_multiply_strassen(MAT* A, MAT* B, MAT* C) {
 
+    int newSize = C->size / 2;
+    MAT A11,A12,A21,A22,B11,B12,B21,B22,C11,C12,C21,C22,M1,M2,M3,M4,M5,M6,M7,AResult,BResult;
+    constr(&A11 ,newSize);
+    constr(&A12 ,newSize);
+    constr(&A21 ,newSize);
+    constr(&A22 ,newSize);
+    constr(&B11 ,newSize);
+    constr(&B12 ,newSize);
+    constr(&B21 ,newSize);
+    constr(&B22 ,newSize);
+    constr(&C11 ,newSize);
+    constr(&C12 ,newSize);
+    constr(&C21 ,newSize);
+    constr(&C22 ,newSize);
+    constr(&M1 ,newSize);
+    constr(&M2 ,newSize);
+    constr(&M3 ,newSize);
+    constr(&M4 ,newSize);
+    constr(&M5 ,newSize);
+    constr(&M6 ,newSize);
+    constr(&M7 ,newSize);
+    constr(&AResult ,newSize);
+    constr(&BResult ,newSize);
+}
 
 int main(){
      srand(time(0));
-     MAT m1;
+     MAT m1,m2,m3;
      int size = 0;
 
      do{
-     printf("napiste rozmer n matice 2^n x 2^n: ");
+     printf("napiste rozmer n matic 2^n x 2^n: ");
      scanf("%d", &size);
      } while(size < 1);
-     
-     printf("%d", (int)pow(2,size));
      
      constr(&m1,(int)pow(2,size));
      fill(&m1);
      show(m1);
+     
+     
+     constr(&m2,(int)pow(2,size));
+     fill(&m2);
+     show(m2);
+     
+     constr(&m3,(int)pow(2,size));
+     mat_multiply_strassen(&m1,&m2,&m3);
+     
+     printf("vysledok nasobenia uvedenych matic:\n");
+     show(m3);
      destr(&m1);
+     destr(&m2);
+     destr(&m3);
 }
 
 
