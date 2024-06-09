60adba6556c69c211662a2c99c944f8deb21fbd9
midiachok <162707182+midiachok@users.noreply.github.com>
2024-06-09 18:27:39 +0200

    rozdelenie matic

--- E2.strassen.c
+++ E2.strassen.c
@@ -65,31 +65,48 @@ void show(MAT mat) {
 
 }
 
+
+
 void mat_multiply_strassen(MAT* A, MAT* B, MAT* C) {
 
-    int newSize = C->size / 2;
-    MAT A11,A12,A21,A22,B11,B12,B21,B22,C11,C12,C21,C22,M1,M2,M3,M4,M5,M6,M7,AResult,BResult;
-    constr(&A11 ,newSize);
-    constr(&A12 ,newSize);
-    constr(&A21 ,newSize);
-    constr(&A22 ,newSize);
-    constr(&B11 ,newSize);
-    constr(&B12 ,newSize);
-    constr(&B21 ,newSize);
-    constr(&B22 ,newSize);
-    constr(&C11 ,newSize);
-    constr(&C12 ,newSize);
-    constr(&C21 ,newSize);
-    constr(&C22 ,newSize);
-    constr(&M1 ,newSize);
-    constr(&M2 ,newSize);
-    constr(&M3 ,newSize);
-    constr(&M4 ,newSize);
-    constr(&M5 ,newSize);
-    constr(&M6 ,newSize);
-    constr(&M7 ,newSize);
-    constr(&AResult ,newSize);
-    constr(&BResult ,newSize);
+     int newSize = C->size / 2;
+     MAT A11,A12,A21,A22,B11,B12,B21,B22,C11,C12,C21,C22,M1,M2,M3,M4,M5,M6,M7,AResult,BResult;
+     constr(&A11 ,newSize);
+     constr(&A12 ,newSize);
+     constr(&A21 ,newSize);
+     constr(&A22 ,newSize);
+     constr(&B11 ,newSize);
+     constr(&B12 ,newSize);
+     constr(&B21 ,newSize);
+     constr(&B22 ,newSize);
+     constr(&C11 ,newSize);
+     constr(&C12 ,newSize);
+     constr(&C21 ,newSize);
+     constr(&C22 ,newSize);
+     constr(&M1 ,newSize);
+     constr(&M2 ,newSize);
+     constr(&M3 ,newSize);
+     constr(&M4 ,newSize);
+     constr(&M5 ,newSize);
+     constr(&M6 ,newSize);
+     constr(&M7 ,newSize);
+     constr(&AResult ,newSize);
+     constr(&BResult ,newSize);
+    
+     /*rozdelme matice A a B*/
+     for (int i = 0; i < newSize; i++) {
+          for (int j = 0; j < newSize; j++) {
+               A11.matrix[i][j] = A->matrix[i][j];
+               A12.matrix[i][j] = A->matrix[i][j + newSize];
+               A21.matrix[i][j] = A->matrix[i + newSize][j];
+               A22.matrix[i][j] = A->matrix[i + newSize][j + newSize];
+
+               B11.matrix[i][j] = B->matrix[i][j];
+               B12.matrix[i][j] = B->matrix[i][j + newSize];
+               B21.matrix[i][j] = B->matrix[i + newSize][j];
+               B22.matrix[i][j] = B->matrix[i + newSize][j + newSize];
+          }
+     }
 }
 
 int main(){
